ea7744488bd8808292673e98b2057e1de44e6ad1
maxwellowuor <myemail.com>
2023-02-21 23:55:23 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Retuen: void
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
